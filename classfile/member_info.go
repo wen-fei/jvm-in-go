2492f1320b27defff5307e72f34b928c4236928d
wen-fei <282699766@qq.com>
2019-11-24 14:52:37 +0800

    member info interface

---
+++ classfile/member_info.go
@@ -0,0 +1,43 @@
+package classfile
+
+type MemberInfo struct {
+	cp              uint16
+	accessIndex     uint16
+	nameIndex       uint16
+	descriptorIndex uint16
+	attributes      []AttributeInfo
+}
+
+func readMembers(reader *ClassReader, cp ConstantPool) []*MemberInfo {
+	memberCount := reader.readUint16()
+	members := make([]*MemberInfo, memberCount)
+	for i := range members {
+		members[i] = readMember(reader, cp)
+	}
+	return members
+}
+
+// read field or method information
+func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
+	return &MemberInfo{
+		cp:              cp,
+		accessIndex:     reader.readUint16(),
+		nameIndex:       reader.readUint16(),
+		descriptorIndex: reader.readUint16(),
+		attributes:      readAttributes(reader, cp),
+	}
+}
+
+func (self *MemberInfo) AccessFlags() uint16 {
+
+}
+
+// find field or method name from constant pool
+func (self *MemberInfo) Name() string {
+	return self.cp.getUtf8(self.nameIndex)
+}
+
+// find field or method descriptor from constant pool
+func (self *MemberInfo) Descriptor() string {
+	return self.cp.getUtf8(self.descriptorIndex)
+}
