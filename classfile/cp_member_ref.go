20dba432033f30521d6c6e279ef0d4d2d94096d3
wen-fei <282699766@qq.com>
2019-11-24 20:32:00 +0800

    add comment

--- classfile/cp_member_ref.go
+++ classfile/cp_member_ref.go
@@ -19,6 +19,7 @@ func (self *ConstantMemberrefInfo) NameAndDescriptor() (string, string) {
 	return self.cp.getNameAndType(self.nameAndTypeIndex)
 }
 
+// 结构体嵌套模拟继承
 // field symbol ref
 type ConstantFieldrefInfo struct {
 	ConstantMemberrefInfo
