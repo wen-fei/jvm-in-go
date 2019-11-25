8b94466320ea8eaa32169f15a54817492bef06b1
wen-fei <282699766@qq.com>
2019-11-25 08:59:44 +0800

    attribute_info table

---
+++ classfile/attribute_info.go
@@ -0,0 +1,51 @@
+package classfile
+
+// attribute_info table
+type AttributeInfo interface {
+	readInfo(reader *ClassReader)
+}
+
+// read method attributes info
+func readAttributes(reader *ClassReader, cp ConstantPool) []AttributeInfo {
+	attributesCount := reader.readUint16()
+	attributes := make([]AttributeInfo, attributesCount)
+	for i := range attributes {
+		attributes[i] = readAttribue(reader, cp)
+	}
+	return attributes
+}
+
+// read single attribute_info table
+func readAttribute(reader *ClassReader, cp ConstantPool) AttributeInfo {
+	attrNameIndex := reader.readUint16()
+	// get attribute_info from constant pool
+	attrName := cp.getUtf8(attrNameIndex)
+	attrLen := reader.readUint32()
+	// create attribute_info object
+	attrInfo := newAttributeInfo(attrName, attrLen, cp)
+	attrInfo.readInfo(reader)
+	return attrInfo
+}
+
+func newAttributeInfo(attrName string, attrLen uint32, cp ConstantPool) AttributeInfo {
+	switch attrName {
+	case "Code":
+		return &CodeAttribute{cp: cp}
+	case "ConstantValue":
+		return &COnstantValueAttribute{}
+	case "Deprecated":
+		return &DeprecatedAttribute{}
+	case "Exceptions":
+		return &ExceptionsAttribute{}
+	case "LineNumberTable":
+		return &LineNumberTableAttribute{}
+	case "LocalVariableTable":
+		return &LocalVariableTableAttribute{}
+	case "SourceFile":
+		return &SourceFileAttribute{cp: cp}
+	case "Synthetic":
+		return &SyntheticAttribue{}
+	default:
+		return &UnparsedAttribute{attrName, attrLen, nil}
+	}
+}
