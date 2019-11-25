f82c0694c59ea0e40ab2a32929c24d1b7ffbda2b
wen-fei <282699766@qq.com>
2019-11-25 15:26:58 +0800

    class file test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"jvm-in-go/classfile"
 	"jvm-in-go/classpath"
 	"strings"
 )
@@ -23,15 +24,51 @@ func startJVM(cmd *Cmd) {
 	//fmt.Printf("classpath: %s class: %s args: %v\n",
 	//	cmd.cpOption, cmd.class, cmd.args)
 
-	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
-	fmt.Printf("classpath:%v class: %v args:%v\n",
-		cp, cmd.class, cmd.args)
+	//cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	//fmt.Printf("classpath:%v class: %v args:%v\n",
+	//	cp, cmd.class, cmd.args)
+	//
+	//className := strings.Replace(cmd.class, ".", "/", -1)
+	//classData, _, err := cp.ReadClass(className)
+	//if err != nil {
+	//	fmt.Printf("Could not find or load main class %s\n", cmd.class)
+	//	return
+	//}
+	//fmt.Printf("class data:%v\n", classData)
 
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
 	className := strings.Replace(cmd.class, ".", "/", -1)
+	cf := loadClass(className, cp)
+	fmt.Println(className, cp)
+	printClassInfo(cf)
+}
+
+func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
 	classData, _, err := cp.ReadClass(className)
 	if err != nil {
-		fmt.Printf("Could not find or load main class %s\n", cmd.class)
-		return
+		panic(err)
 	}
-	fmt.Printf("class data:%v\n", classData)
+	cf, err := classfile.Parse(classData)
+	if err != nil {
+		panic(err)
+	}
+	return cf
+}
+
+func printClassInfo(cf *classfile.ClassFile) {
+	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
+	fmt.Printf("constans count: %v\n", len(cf.ConstantPool()))
+	fmt.Printf("access flags: %v\n", cf.AccessFlags())
+	fmt.Printf("this class: %v\n", cf.ClassName())
+	fmt.Printf("super class: %v\n", cf.SuperClassName())
+	fmt.Printf("interface: %v\n", cf.InterfaceNames())
+	fmt.Printf("fields count: %v\n", len(cf.Fields()))
+
+	for _, f := range cf.Fields() {
+		fmt.Printf("    %s\n", f.Name())
+	}
+	fmt.Printf("Methods count: %v\n", len(cf.Methods()))
+	for _, m := range cf.Methods() {
+		fmt.Printf("    %s\n", m.Name())
+	}
 }
