be2f349f4d7c12db22e018a0d4789dad87782028
wen-fei <282699766@qq.com>
2019-11-25 14:37:25 +0800

    Constant Invoke Dynamic Info

--- classfile/cp_invoke_dynamic.go
+++ classfile/cp_invoke_dynamic.go
@@ -9,3 +9,17 @@ func (self *ConstantMethodHandleInfo) readInfo(reader *ClassReader) {
 	self.referenceKind = reader.readUint8()
 	self.referenceIndex = reader.readUint16()
 }
+
+type ConstantMethodTypeInfo struct {
+	descriptorIndex uint16
+}
+
+type ConstantInvokeDynamicInfo struct {
+	bootstrapMethodAttrIndex uint16
+	nameAndTypeIndex         uint16
+}
+
+func (self *ConstantInvokeDynamicInfo) readInfo(reader *ClassReader) {
+	self.bootstrapMethodAttrIndex = reader.readUint16()
+	self.nameAndTypeIndex = reader.readUint16()
+}
