f04770fbf84fdfd3b0e2260959dc2337dad69241
wen-fei <282699766@qq.com>
2019-12-05 14:26:11 +0800

    jvm stack

---
+++ rtda/jvm-stack.go
@@ -0,0 +1,44 @@
+package rtda
+
+type Stack struct {
+	maxSize uint
+	// the current size of stack
+	size uint
+	_top *Frame
+}
+
+func newStack(maxSize uint) *Stack {
+	return &Stack{
+		maxSize: maxSize,
+	}
+}
+
+func (self *Stack) push(frame *Frame) {
+	if self.size >= self.maxSize {
+		panic("java.lang.StackOverflowError")
+	}
+	if self._top != nil {
+		frame.lower = self._top
+	}
+
+	self._top = frame
+	self.size++
+}
+
+func (self *Stack) pop() *Frame {
+	if self._top == nil {
+		panic("jvm stack is empty!")
+	}
+	top := self._top
+	self._top = top.lower
+	self.size--
+	return top
+}
+
+func (self *Stack) top() *Frame {
+	if self._top == nil {
+		panic("jvm stack is empty!")
+	}
+	return self._top
+
+}
