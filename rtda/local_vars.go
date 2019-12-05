0638356a4518a30710e318558b32202563d8240e
wen-fei <282699766@qq.com>
2019-12-05 14:43:16 +0800

    局部变量表

---
+++ rtda/local_vars.go
@@ -0,0 +1,43 @@
+package rtda
+
+import "math"
+
+type LocalVars []Slot
+
+func newLocalVars(maxLocals uint) LocalVars {
+	if maxLocals > 0 {
+		return make([]Slot, maxLocals)
+	}
+	return nil
+}
+
+func (self LocalVars) SetInt(index uint, val int32) {
+	self[index].num = val
+}
+
+func (self LocalVars) GetInt(index uint) int32 {
+	return self[index].num
+}
+
+// float可以先转成int类型，再按int变量来处理
+func (self LocalVars) SetFloat(index uint, val float32) {
+	bits := math.Float32bits(val)
+	self[index].num = int32(bits)
+}
+
+func (self LocalVars) GetFloat(index uint) float32 {
+	bits := uint32(self[index].num)
+	return math.Float32frombits(bits)
+}
+
+// long变量需要拆成两个int变量
+func (self LocalVars) SetLong(index uint, val int64) {
+	self[index].num = int32(val)
+	self[index+1].num = int32(val >> 32)
+}
+
+func (self LocalVars) GetLong(index uint) int64 {
+	low := uint32(self[index].num)
+	high := uint32(self[index+1].num)
+	return int64(high)>>32 | int64(low)
+}
