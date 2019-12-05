83537cc27122262cab25970efa91284501ba00b1
wen-fei <282699766@qq.com>
2019-12-05 20:30:12 +0800

    instruction

---
+++ instructions/base/instruction.go
@@ -0,0 +1,44 @@
+package base
+
+import "github.com/jvm-in-go/rtda"
+
+// 很多指令操作数类似，因此定义一个base接口，类似Java中的抽象类
+type Instruction interface {
+	// 从字节码中提取操作数
+	FetchOperands(reader *BytecodeReader)
+	// 执行指令逻辑
+	Execute(frame *rtda.Frame)
+}
+
+type NoOperandsInstruction struct {
+}
+
+func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader) {
+	//nothing to do
+}
+
+// 跳转指令
+type BranchInstruction struct {
+	Offset int
+}
+
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader) {
+	self.Offset = int(reader.ReadUint16())
+}
+
+// 存储和加载指令，根据索引存取局部变量表
+type Index8Intstruction struct {
+	Index int
+}
+
+func (self *Index8Intstruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint8())
+}
+
+type Index16Instruction struct {
+	Index int
+}
+
+func (self *Index16Instruction) FetchOperands(reader *BytecodeReader) {
+	self.Index = uint(reader.ReadUint16())
+}
