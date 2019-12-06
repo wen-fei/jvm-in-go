9fbb7b2ad8391322c0b97845ccf1e4e41ba37478
wen-fei <282699766@qq.com>
2019-12-06 21:49:30 +0800

    load instructions

--- instructions/loads/iload.go
+++ instructions/loads/iload.go
@@ -30,3 +30,23 @@ func _iload(frame *rtda.Frame, index uint) {
 	val := frame.LocalVars().GetInt(index)
 	frame.OperandStack().PushInt(val)
 }
+
+func (self *ILOAD) Execute(frame *rtda.Frame) {
+	_iload(frame, uint(self.Index))
+}
+
+func (self *ILOAD_0) Execute(frame *rtda.Frame) {
+	_iload(frame, 0)
+}
+
+func (self *ILOAD_1) Execute(frame *rtda.Frame) {
+	_iload(frame, 1)
+}
+
+func (self *ILOAD_2) Execute(frame *rtda.Frame) {
+	_iload(frame, 2)
+}
+
+func (self *ILOAD_3) Execute(frame *rtda.Frame) {
+	_iload(frame, 3)
+}
