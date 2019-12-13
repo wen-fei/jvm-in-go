d7674e355b5cb167fabef6aeeed4b59da3129799
tengyun <tengyun.cs.zhang@gmail.com>
2019-12-13 22:14:07 +0800

    Frame struct add new two var

--- rtda/frame.go
+++ rtda/frame.go
@@ -6,10 +6,14 @@ type Frame struct {
 	localVars LocalVars
 	// 操作数栈指针
 	operandStack *OperandStack
+
+	thread *Thread
+	nextPc int
 }
 
-func NewFrame(maxLocals, maxStack uint) *Frame {
+func newFrame(thread *Thread, maxLocals, maxStack uint) *Frame {
 	return &Frame{
+		thread:       thread,
 		localVars:    newLocalVars(maxLocals),
 		operandStack: newOperandStack(maxStack),
 	}
