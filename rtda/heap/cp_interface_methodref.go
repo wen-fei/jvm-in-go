8504a0fe10365ac210bbcba2e50ec20b7f734efc
tengyun <282699766@qq.com>
2019-12-15 12:19:38 +0800

    接口方法符号引用

---
+++ rtda/heap/cp_interface_methodref.go
@@ -0,0 +1,15 @@
+package heap
+
+import "github.com/jvm-in-go/classfile"
+
+type InterfaceMethodRef struct {
+	MemberRef
+	method *Method
+}
+
+func newInterfaceMethodRef(cp *ConstantPool, refInfo *classfile.ConstantInterfaceMethodrefInfo) *InterfaceMethodRef {
+	ref := &InterfaceMethodRef{}
+	ref.cp = cp
+	ref.copyMemberRefInfo(&refInfo.ConstantMemberrefInfo)
+	return ref
+}
