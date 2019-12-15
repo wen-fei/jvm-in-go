4ae5ce1d39e5b0c2611fc51ab364dea6d27aa939
tengyun <282699766@qq.com>
2019-12-15 12:03:37 +0800

    从class文件内存储的字段或方法常量中提取数据

---
+++ rtda/heap/cp_memberref.go
@@ -0,0 +1,14 @@
+package heap
+
+import "github.com/jvm-in-go/classfile"
+
+type MemberRef struct {
+	SymRef
+	name       string
+	descriptor string
+}
+
+func (self *MemberRef) copyMemberRefInfo(refInfo *classfile.ConstantMemberrefInfo) {
+	self.className = refInfo.ClassName()
+	self.name, self.descriptor = refInfo.NameAndDescriptor()
+}
