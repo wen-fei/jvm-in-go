15a3d7941116568254e9a13e1c6f8144a58bdb08
tengyun <282699766@qq.com>
2019-12-17 10:46:22 +0800

    add necessary getter method

--- rtda/heap/field.go
+++ rtda/heap/field.go
@@ -20,3 +20,15 @@ func newFields(class *Class, cfFields []*classfile.MemberInfo) []*Field {
 func (self *Field) isLongOrDouble() bool {
 	return self.descriptor == "J" || self.descriptor == "D"
 }
+
+func (self *Field) SlotId() uint {
+	return self.slotId
+}
+
+func (self *Field) ConstValueIndex() uint {
+	return 0
+}
+
+func (self *Field) Descriptor() string {
+	return self.descriptor
+}
