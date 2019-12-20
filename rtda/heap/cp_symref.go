c7a2a09471bcf5e9cf61dd00f2b69f1eda3ddb8b
tengyun <282699766@qq.com>
2019-12-20 21:44:05 +0800

    类符号引用解析

--- rtda/heap/cp_symref.go
+++ rtda/heap/cp_symref.go
@@ -6,3 +6,21 @@ type SymRef struct {
 	className string
 	class     *Class
 }
+
+// 类符号引用解析
+func (self *SymRef) ResolvedClass() *Class {
+	if self.class == nil {
+		// 如果类符号引用已经解析，直接返回类指针，否则调用resolveClassRef
+		self.resolveClassRef()
+	}
+	return self.class
+}
+
+func (self *SymRef) resolveClassRef() {
+	d := self.cp.class
+	c := d.loader.LoadClass(self.className)
+	if !c.isAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+	self.class = c
+}
