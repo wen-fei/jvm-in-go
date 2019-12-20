0c61fd18d0f27b7ba0e3bfba29395944bbc53dd4
tengyun <282699766@qq.com>
2019-12-20 23:03:23 +0800

    need todo

---
+++ instructions/references/new.go
@@ -0,0 +1,22 @@
+package references
+
+import (
+	"github.com/jvm-in-go/instructions/base"
+	"github.com/jvm-in-go/rtda"
+	"github.com/jvm-in-go/rtda/heap"
+)
+
+type NEW struct {
+	base.Index16Instruction
+}
+
+func (self *NEW) Execute(frame *rtda.Frame) {
+	cp := frame.Method().Class().ConstantPool()
+	classRef := cp.GetConstant(self.Index).(*heap.ClassRef)
+	class := classRef.ResolvedClass()
+	if class.IsInterface() || class.IsAbstract() {
+		panic("java.lang.InstantiationError")
+	}
+	ref := class.NewObject()
+	frame.OperandStack().PushRef(ref)
+}
