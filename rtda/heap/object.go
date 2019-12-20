c363edd220dcefe3c3c36773854df416f0355036
tengyun <282699766@qq.com>
2019-12-20 22:49:53 +0800

    add newObject()

--- rtda/heap/object.go
+++ rtda/heap/object.go
@@ -4,3 +4,10 @@ type Object struct {
 	class  *Class
 	fields Slots
 }
+
+func newObject(class *Class) *Object {
+	return &Object{
+		class:  class,
+		fields: newSlots(class.instanceSlotCount),
+	}
+}
