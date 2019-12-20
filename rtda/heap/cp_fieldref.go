8a761981070cfefb1089b17c5612b57c6325be61
tengyun <282699766@qq.com>
2019-12-20 22:33:53 +0800

    lookupField

--- rtda/heap/cp_fieldref.go
+++ rtda/heap/cp_fieldref.go
@@ -13,3 +13,44 @@ func newFieldRef(cp *ConstantPool, refInfo *classfile.ConstantFieldrefInfo) *Fie
 	ref.copyMemberRefInfo(&refInfo.ConstantMemberrefInfo)
 	return ref
 }
+
+// 字段符号引用解析
+func (self *FieldRef) ResolvedField() *Field {
+	if self.field == nil {
+		self.resolvedFieldRef()
+	}
+	return self.field
+}
+
+func (self *FieldRef) resolvedFieldRef() {
+	d := self.cp.class
+	c := self.ResolvedClass()
+	field := lookupField(c, self.name, self.descriptor)
+	if field == nil {
+		panic("java.lang.NoSuchFieldError")
+	}
+	if !field.isAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+	self.field = field
+}
+
+func lookupField(c *Class, name string, descriptor string) *Field {
+	// 查找当前类
+	for _, field := range c.fields {
+		if field.name == name && field.descriptor == descriptor {
+			return field
+		}
+	}
+	// 递归查找接口
+	for _, iface := range c.interfaces {
+		if field := lookupField(iface, name, descriptor); field != nil {
+			return field
+		}
+	}
+	// 查找超类
+	if c.superClass != nil {
+		return lookupField(c.superClass, name, descriptor)
+	}
+	return nil
+}
