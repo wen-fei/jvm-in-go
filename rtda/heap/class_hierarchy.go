277748f8abc80422f18693577b01daa379004cad
tengyun <282699766@qq.com>
2019-12-21 12:19:12 +0800

    isSubInterfaceOf

---
+++ rtda/heap/class_hierarchy.go
@@ -0,0 +1,35 @@
+package heap
+
+// 在三种情况下，S类型的引用值可以赋值给T类型：S和T是同一类型；T是类且S是T的子类；或者T是接口且S实现了T接口。
+func (self *Class) isAssignableFrom(other *Class) bool {
+	s, t := other, self
+	if s == t {
+		return true
+	}
+
+	if !t.IsInterface() {
+		return s.isSubClassOf(t)
+	} else {
+		return s.isImplements(t)
+	}
+}
+
+func (self *Class) isImplements(iface *Class) bool {
+	for c := self; c != nil; c = c.superClass {
+		for _, i := range c.interfaces {
+			if i == iface || i.isSubInterfaceOf(iface) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (self *Class) isSubInterfaceOf(iface *Class) bool {
+	for _, superInterface := range self.interfaces {
+		if superInterface == iface || superInterface.isSubInterfaceOf(iface) {
+			return true
+		}
+	}
+	return false
+}
