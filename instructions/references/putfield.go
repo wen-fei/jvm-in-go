fd23085125cfb160228da5e056988d2c7dd02fcc
tengyun <282699766@qq.com>
2019-12-21 11:22:45 +0800

    putfield

---
+++ instructions/references/putfield.go
@@ -0,0 +1,72 @@
+package references
+
+import (
+	"github.com/jvm-in-go/instructions/base"
+	"github.com/jvm-in-go/rtda"
+	"github.com/jvm-in-go/rtda/heap"
+)
+
+type PUT_FIELD struct {
+	base.Index16Instruction
+}
+
+func (self *PUT_FIELD) Execute(frame *rtda.Frame) {
+	currentMethod := frame.Method()
+	currentClass := currentMethod.Class()
+	cp := currentClass.ConstantPool()
+	fieldRef := cp.GetConstant(self.Index).(*heap.FieldRef)
+	field := fieldRef.ResolvedField()
+	// 解析后的字段必须是实例字段，否则抛出IncompatibleClassChangeError
+	if field.IsStatic() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	// 如果是final字段，则只能在构造函数中初始化，否则抛出IllegalAccessError
+	if field.IsFinal() {
+		if currentClass != field.Class() || currentMethod.Name() != "<init>" {
+			panic("java.lang.IllegalAccessError")
+		}
+	}
+
+	descriptor := field.Descriptor()
+	slotId := field.SlotId()
+	stack := frame.OperandStack()
+	switch descriptor[0] {
+	case 'Z', 'B', 'C', 'S', 'I':
+		val := stack.PopInt()
+		ref := stack.PopRef()
+		if ref == nil {
+			panic("java.lang.NullPointerException")
+		}
+		ref.Fields().SetInt(slotId, val)
+	case 'F':
+		val := stack.PopFloat()
+		ref := stack.PopRef()
+		if ref == nil {
+			panic("java.lang.NullPointerException")
+		}
+		ref.Fields().SetFloat(slotId, val)
+	case 'J':
+		val := stack.PopLong()
+		ref := stack.PopRef()
+		if ref == nil {
+			panic("java.lang.NullPointerException")
+		}
+		ref.Fields().SetLong(slotId, val)
+	case 'D':
+		val := stack.PopDouble()
+		ref := stack.PopRef()
+		if ref == nil {
+			panic("java.lang.NullPointerException")
+		}
+		ref.Fields().SetDouble(slotId, val)
+	case 'L', '[':
+		val := stack.PopRef()
+		ref := stack.PopRef()
+		if ref == nil {
+			panic("java.lang.NullPointerException")
+		}
+		ref.Fields().SetRef(slotId, val)
+	default:
+		// todo
+	}
+}
