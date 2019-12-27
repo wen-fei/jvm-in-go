632a5ccb4e9d232f44d02ed07ee4469db8aeb505
wen-fei <282699766@qq.com>
2019-12-27 20:58:58 +0800

    non-interface method resolve

--- rtda/heap/cp_methodref.go
+++ rtda/heap/cp_methodref.go
@@ -13,3 +13,36 @@ func newMethodRef(cp *ConstantPool, refInfo *classfile.ConstantMethodrefInfo) *M
 	ref.copyMemberRefInfo(&refInfo.ConstantMemberrefInfo)
 	return ref
 }
+
+func (self *MethodRef) ResolvedMethod() *Method {
+	// 如果还没有解析过符号引用
+	if self.method == nil {
+		self.resolveMethodRef()
+	}
+	return self.method
+}
+
+func (self *MethodRef) resolveMethodRef() {
+	d := self.cp.class
+	c := self.ResolvedClass()
+	if c.IsInterface() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	method := lookupMethod(c, self.name, self.descriptor)
+	if method == nil {
+		panic("java.lang.NoSuchMethodError")
+	}
+	if !method.isAccessibleTo(d) {
+		panic("java.lang.IllegalAccessError")
+	}
+	self.method = method
+}
+
+func lookupMethod(class *Class, name, descriptor string) *Method {
+	// 先从c的继承层次找，找不到从接口中找
+	method := LookupMethodInClass(class, name, descriptor)
+	if method == nil {
+		method = lookupMethodInInterfaces(class.interfaces, name, descriptor)
+	}
+	return method
+}
