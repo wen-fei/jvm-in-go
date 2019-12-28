9c137d227aa99fe6009fa62ccf042447aa8c4ec2
wen-fei <282699766@qq.com>
2019-12-28 21:52:04 +0800

    invokestatic

---
+++ instructions/references/invokestatic.go
@@ -0,0 +1,22 @@
+package references
+
+import (
+	"github.com/jvm-in-go/instructions/base"
+	"github.com/jvm-in-go/rtda"
+	"github.com/jvm-in-go/rtda/heap"
+)
+
+type INVOKE_STATIC struct {
+	base.Index16Instruction
+}
+
+func (self *INVOKE_STATIC) Execute(frame *rtda.Frame) {
+	cp := frame.Method().Class().ConstantPool()
+	methodRef := cp.GetConstant(self.Index).(*heap.MethodRef)
+	resolvedMethod := methodRef.ResolvedMethod()
+	if !resolvedMethod.IsStatic() {
+		panic("java.lang.IncompatibleClassChangeError")
+	}
+	base.InvokeMethod(frame, resolvedMethod)
+
+}
